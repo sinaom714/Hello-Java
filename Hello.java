cdbc7cc8e76ea52903b0c153ee8a6dc87d65eeed
sinaom714 <sinaom714@gmail.com>
2025-09-05 16:27:34 +0700

    Add Hello Java application

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+        System.out.println("สวัสดี Java!");
+        System.out.println("This is a simple Java Hello World application");
+    }
+}
